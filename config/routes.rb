22854204cbab4be7a76621e111ad9b3aebbcbe14
sho endo <endo.sho@moneyforward.co.jp>
2020-04-21 18:23:31 +0900

    Add endpoint for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#logged_in'
   root to: 'static#home'
 end
