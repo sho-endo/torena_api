8f06fd042c0cbba07af1436859f3d7a7f4cb15a5
sho endo <endo.sho@moneyforward.co.jp>
2020-05-01 19:14:52 +0900

    Modify static request spec

--- spec/requests/static_request_spec.rb
+++ spec/requests/static_request_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe "Statics", type: :request do
 
   describe "GET /" do
-    it "returns http success" do
-      get "/"
-      expect(response).to have_http_status(:success)
+    it 'should return "Hello World"' do
+      is_expected.to eq(200)
+
+      res = JSON.parse(response.body)
+      expect(res["message"]).to eq("Hello World")
     end
   end
 end
