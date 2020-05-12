36fa9bd5616de1c0b2c27eb55a867bd0eb36cf89
sho endo <endo.sho@moneyforward.co.jp>
2020-05-12 09:14:09 +0900

    Add Not Null Constraint and unique index to users table

---
+++ db/migrate/20200511235929_change_users_columns_not_null.rb
@@ -0,0 +1,7 @@
+class ChangeUsersColumnsNotNull < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :users, :email, false
+    change_column_null :users, :password_digest, false
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_090718) do
+ActiveRecord::Schema.define(version: 2020_05_11_235929) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
