40afd734ecc5b2270080eb1748dd40740a6e114f
sho endo <endo.sho@moneyforward.co.jp>
2020-05-14 10:07:59 +0900

    Add create action to menus controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,28 @@
 class MenusController < ApplicationController
+  before_action :authenticate_user
+
   def create
+    part = current_user.parts.find_by(id: params[:part_id])
+    unless part
+      render json: { message: 'Bad Request' }, status: 400 and return
+    end
+
+    menu = part.menus.build(menu_params)
+    if menu.save
+      render json: {
+        message: "#{menu.name}を追加しました",
+        menu: menu
+      }, status: 200
+    else
+      render json: {
+        message: menu.errors.full_messages
+      }, status: 400
+    end
   end
+
+  private
+
+    def menu_params
+      params.require(:menu).permit(:name)
+    end
 end
